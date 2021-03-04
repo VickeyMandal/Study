4ad7ecd0b639cad8b0186444eeb9d6a9101c5185
VickeyMandal <mandalvickey@gmail.com>
2021-03-04 21:00:20 +0530

    UP

---
+++ Sem4/CSE310 JAVA/Programs/datee.java
@@ -0,0 +1,10 @@
+class a{}
+class Parent extends a{
+
+            public static void main(String[] rk) {
+
+                a obj = new Parent();
+                System.out.println(a instanceof Parent);
+            }
+        }
+
